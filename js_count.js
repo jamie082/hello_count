522ca3a7a97914234e35a725ba1eccea42208a6e
jamie082 <jamiemorrissey02@gmail.com>
2022-10-20 18:15:15 -0700

    added source

---
+++ js_count.js
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <script langauge= "javascript" type = "text/javascript">
+            <!--
+            var count = 1;
+                alert("This is the start of the web site")
+
+                while (count <= 10)
+                {
+                    document.write("Current count : " + count + "<br />");
+                    count++;
+                }
+                document.write("Loop stopped!")
+                -->
+        </script>
+    </body>
+</html>
